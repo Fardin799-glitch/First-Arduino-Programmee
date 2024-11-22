ca6f199a862c18979658e69025663ecdb5327959
Fardin799-glitch <fardinchilled@gmail.com>
2024-11-22 18:25:40 +0600

    Add files via upload

---
+++ neat_blad_wluff1 (1).ino
@@ -0,0 +1,16 @@
+// C++ code
+//
+void setup()
+{
+ Serial.begin(9600);
+  int x=10,y=5,sum;
+  sum=x+y;
+  Serial.println(sum);
+ 
+}
+
+void loop()
+{
+  //Serial.println("Aksalamualikum \nFardin");
+
+}
---
+++ neat_blad_wluff1.ino
@@ -0,0 +1,18 @@
+// C++ code
+//
+void setup()
+{
+ Serial.begin(9600);
+  int x;
+  x=10;
+  int y;
+  y=10;
+  Serial.println(x);
+  Serial.println(y);
+}
+
+void loop()
+{
+  //Serial.println("Aksalamualikum \nFardin");
+
+}
